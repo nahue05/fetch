f55a910b998fdb459f0b5b9b57b87333c885a0bb
nahuel <christiannahuelberrutti25@gmail.com>
2022-08-08 22:26:48 -0300

    consulta de erroes

--- catalogo.js
+++ catalogo.js
@@ -1,13 +1,13 @@
 const lista = document.getElementById("lista");
-fetch("./catalogo.json")
+fetch("../catalogo.jsS")
 .then(resp => resp.json())
 .then(productos => {
-    productos.foreach(producto => {
+    productos.forEach(producto => {
         const li = document.createElement("li");
         li.innerHTML = `
-        <h1>${producto.marca}</h1>
-        <img src="${producto.imagen}">
-        <h3>Precio por dia:${producto.precio}</h3>
+        <h1>${producto.Marca}</h1>
+        <img src="${producto.Imagen}">
+        <h3>Precio por dia:${producto.Precio}</h3>
         <hr/>
         `;
         lista.append(li)
